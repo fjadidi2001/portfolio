4846f4dbf77b8c16a1ad68bb96020b00d7c9ccc9
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-15 10:43:52 +0330

    added views

--- jobsite/jobinfo/views.py
+++ jobsite/jobinfo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import JobApplication
 
-# Create your views here.
+
+def job_list(request):
+    applications = JobApplication.objects.all()
+    return render(request, 'jobinfo/job_list.html', {'applications': applications})
