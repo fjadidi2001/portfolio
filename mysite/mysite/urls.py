36d0747536461edcd2eabdea0974221b4787ed9c
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-15 17:46:52 +0330

    config again urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-
-
-from mysite.resume.views import resume
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('resume/', resume),
+    path('admin/', admin.site.urls),
+    path('resume/', include('resume.urls')),
 ]
 
