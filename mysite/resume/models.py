de1cfbabe2d2f88e65ebc453d8f2cc8824604bd9
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-15 17:29:00 +0330

    config models

--- mysite/resume/models.py
+++ mysite/resume/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Resume(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    skills = models.TextField()
+    experience = models.TextField()
+    education = models.TextField()
+
+    def __str__(self):
+        return self.name
