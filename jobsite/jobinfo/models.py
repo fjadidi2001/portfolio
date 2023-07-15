f0a8b84b748b5cf73b698e7f89f8f3a4918adff1
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-15 10:25:22 +0330

    added models

---
+++ jobsite/jobinfo/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class JobApplication(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+    date_applied = models.DateField(auto_now_add=True)
