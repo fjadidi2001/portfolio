54dcb516ec65e35f40973b9219c475b5b16d654b
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-26 13:44:07 +0330

    update models

--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -1,7 +1,37 @@
-
 from django.db import models
+
+
+class Summary(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Education(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class WorkExperience(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Skills(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
 
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+
+
+class AcademicReferences(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class ProfessionalReferences(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -1,9 +1,9 @@
 # portfolio/views.py
 
 from django.shortcuts import render
-from .models import Project
+from .models import Academic_References
 
 
 def portfolio_view(request):
-    projects = Project.objects.all()
+    projects = Academic_References.objects.all()
     return render(request, 'portfolio/portfolio.html', {'projects': projects})
