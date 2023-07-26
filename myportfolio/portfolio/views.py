891f8cb13a701d5d4c4c2ebe0535143b094676f6
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-26 14:00:09 +0330

    update views

--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -1,9 +1,23 @@
 # portfolio/views.py
 
 from django.shortcuts import render
-from .models import Academic_References
+from .models import Summary, Education, WorkExperience, Skills, Project, AcademicReferences, ProfessionalReferences
 
 
 def portfolio_view(request):
-    projects = Academic_References.objects.all()
-    return render(request, 'portfolio/portfolio.html', {'projects': projects})
+    summary = Summary.objects.all()
+    education = Education.objects.all()
+    workExperience = WorkExperience.objects.all()
+    skills = Skills.objects.all()
+    project = Project.objects.all()
+    academicReferences = AcademicReferences.objects.all()
+    professionalReferences = ProfessionalReferences.objects.all()
+    return render(request, 'portfolio/portfolio.html', {
+        'summary': summary,
+        'education': education,
+        'workExperience': workExperience,
+        'skills': skills,
+        'project': project,
+        'academicReferences': academicReferences,
+        'professionalReferences': professionalReferences,
+    })
