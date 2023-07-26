97c87c3c02185b4ad16036b349f688831ac57ef6
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-26 11:31:50 +0330

    update models

--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -6,15 +6,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    # Add more fields like project image, URL, etc.
-
-
-class Skill(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-
-
-class Experience(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    # Add more fields like duration, company name, etc.
+    image = models.ImageField(upload_to='project_images/')
+    # Add more fields as needed
