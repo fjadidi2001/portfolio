444a6a520d0c9b64c69615072af392e1883a3296
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-26 09:59:38 +0330

    added models

--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -1,3 +1,20 @@
+# portfolio/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add more fields like project image, URL, etc.
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add more fields like duration, company name, etc.
