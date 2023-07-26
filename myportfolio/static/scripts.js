a212fdfe7e0bfb52b16d48bf40d34d1f101e9921
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-26 11:40:02 +0330

    update js

--- myportfolio/static/scripts.js
+++ myportfolio/static/scripts.js
@@ -1,18 +1,28 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+// static/scripts.js
 
-// Form submission handling
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Add your form submission logic here
-    // For example, you can use AJAX to send form data to the server
-    // and display a success message to the user.
-});
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to add animation class to visible project cards
+function showVisibleProjectCards() {
+  const projectCards = document.querySelectorAll('.project-card');
+  projectCards.forEach((card) => {
+    if (isInViewport(card)) {
+      card.classList.add('fadeIn');
+    }
+  });
+}
+
+// Event listener for scroll events
+window.addEventListener('scroll', showVisibleProjectCards);
+
+// Run the function on page load
+window.addEventListener('load', showVisibleProjectCards);
