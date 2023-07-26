2916314c6a7f6439422fe3fa974935b9b8bbb097
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-26 11:33:24 +0330

    config views

--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -1,15 +1,9 @@
 # portfolio/views.py
 
 from django.shortcuts import render
-from .models import Project, Skill, Experience
+from .models import Project
 
 
 def portfolio_view(request):
     projects = Project.objects.all()
-    skills = Skill.objects.all()
-    experiences = Experience.objects.all()
-    return render(request, 'portfolio/portfolio.html', {
-        'projects': projects,
-        'skills': skills,
-        'experiences': experiences,
-    })
+    return render(request, 'portfolio/portfolio.html', {'projects': projects})
