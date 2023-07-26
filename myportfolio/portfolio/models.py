e7eb8b0cc5ccb7e3f13ac140ac9b1c8f5ed99e52
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-26 13:35:55 +0330

    update models

--- myportfolio/portfolio/models.py
+++ myportfolio/portfolio/models.py
@@ -5,5 +5,3 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='project_images/')
-    # Add more fields as needed
