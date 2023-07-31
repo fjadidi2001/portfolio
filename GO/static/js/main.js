a254641a67c7cf881b336cda6a09cb44c504d3ac
fjadidi2001 <fjadidi2001@gmail.com>
2023-07-31 16:21:21 +0330

    added some js

--- GO/static/js/main.js
+++ GO/static/js/main.js
@@ -20,3 +20,32 @@ function buildProject() {
 }
 
 buildBtn.addEventListener('click', buildProject);
+// ... (existing code)
+
+// Function to show an alert when the "Build Project" button is clicked
+function buildProject() {
+    fetch('/build')
+        .then(response => response.text())
+        .then(output => alert(output))
+        .catch(error => alert('Failed to build the project.'));
+}
+
+buildBtn.addEventListener('click', buildProject);
+
+// Function to show an alert when the "Animate Me!" button is clicked
+function animateText() {
+    const text = document.querySelector('h2');
+    text.style.transition = 'all 1s ease-in-out';
+    text.style.transform = 'rotateY(360deg) scale(1.2)';
+}
+
+animateBtn.addEventListener('click', animateText);
+
+// Function to redirect to the education page
+function goToEducationPage() {
+    window.location.href = '/education';
+}
+
+// Add an event listener to the "Education" button to redirect to the education page
+const educationBtn = document.getElementById('educationBtn');
+educationBtn.addEventListener('click', goToEducationPage);
